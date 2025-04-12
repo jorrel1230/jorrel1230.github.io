0622b493104f10c4f181ef7cb26a9954e6f84308
Jorrel Rajan <jr8867@princeton.edu>
2025-04-12 11:46:23 -0400

    bored

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1 class="text-3xl font-bold">Jorrel Rajan</h1>
-<p>Developing This with SvelteKit.. tbd</p>
+<h1 class="text-3xl font-bold m-2">Jorrel Rajan</h1>
+<br />
+
+<p>Hi im jorrel</p>
+
+<div class="flex gap-4">
+    <a href="https://github.com/jorrel1230" class="w-min">
+        <div class="bg-slate-100 bg-opacity-50 shadow-md rounded-md p-2 m-2 w-min hover:scale-110 hover:shadow-lg hover:bg-opacity-100 transition-all duration-300">
+            <p>github</p>
+        </div>
+    </a>
+
+    <a href="https://www.linkedin.com/in/jorrelrajan/" class="w-min">
+        <div class="bg-slate-100 bg-opacity-50 shadow-md rounded-md p-2 m-2 w-min hover:scale-110 hover:shadow-lg hover:bg-opacity-100 transition-all duration-300">
+            <p>linkedin</p>
+        </div>
+    </a>
+</div>
