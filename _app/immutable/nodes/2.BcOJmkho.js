e51d9a5215b55044eb03f74e9c6959f00ee9efe8
Jorrel Rajan <jr8867@princeton.edu>
2025-04-12 11:49:10 -0400

    prod

--- _app/immutable/nodes/2.BcOJmkho.js
+++
@@ -1,1 +0,0 @@
-import{t as a,a as e}from"../chunks/CO3rNRti.js";import"../chunks/BbbO2Kgu.js";import{n as p}from"../chunks/b6OtWike.js";var r=a('<h1 class="text-3xl font-bold">Jorrel Rajan</h1> <p>Developing This with SvelteKit.. tbd</p>',1);function l(t){var o=r();p(2),e(t,o)}export{l as component};
